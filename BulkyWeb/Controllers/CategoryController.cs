2cbeb4b6e64e473eed5cc56cc70bf8e01147f449
borgepl <pdeborger@gmail.com>
2023-05-05 11:39:06 +0200

    Bootstrap Theme and Category Create

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,3 +1,5 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
@@ -5,16 +7,36 @@ namespace BulkyWeb.Controllers
     public class CategoryController : Controller
     {
         private readonly ILogger<CategoryController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public CategoryController(ILogger<CategoryController> logger)
+        public CategoryController(ILogger<CategoryController> logger, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _context.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
