a8d4e612a9923bf0b1da7ee2b91f1a5b76533d04
borgepl <pdeborger@gmail.com>
2023-05-16 13:56:56 +0200

    emailsender with SMTP mail - MailKit

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace Bulky.Utility
 {
@@ -11,7 +13,26 @@ namespace Bulky.Utility
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             // logic to send email - still to be implemented
+            // return Task.CompletedTask;
+
+            // prepare email
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("eacea-lsa@ec.europa.eu"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage};
+
+            // send the email smtp
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("mail-internal.eacea.cec.eu.int", 25, MailKit.Security.SecureSocketOptions.None);
+                //emailClient.Authenticate("user@test.com","Password");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
+
         }
     }
 }
