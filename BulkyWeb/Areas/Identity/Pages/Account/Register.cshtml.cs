79bcea14cea0ceaad77126b9e1160bac3bbecaed
borgepl <pdeborger@gmail.com>
2023-05-19 16:47:29 +0200

    Create Admin accounts - Register page

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -190,7 +190,12 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if(User.IsInRole(SD.Role_Admin)) {
+                            TempData["success"] = "New User created successfully";
+                        } else {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
+                        
                         return LocalRedirect(returnUrl);
                     }
                 }
