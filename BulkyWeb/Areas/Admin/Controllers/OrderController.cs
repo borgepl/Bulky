800f6627e5e443ded49db5b16793cbeb2d50904a
borgepl <pdeborger@gmail.com>
2023-05-19 12:29:01 +0200

    Update Order Details

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 using Bulky.DataAccess.UoW;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers
@@ -11,6 +13,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
         private readonly ILogger<OrderController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
+        [BindProperty]
+        public OrderVM orderVM { get; set; }
         public OrderController(ILogger<OrderController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
@@ -20,6 +24,42 @@ namespace BulkyWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Details(int orderId)
+        {
+            orderVM = new() {
+                OrderHeader = await _unitOfWork.OrderHeader.GetAsync(u => u.Id == orderId, includeProperties:"ApplicationUser"),
+                OrderDetail = await _unitOfWork.OrderDetail.GetAllAsync(u => u.OrderHeaderId == orderId, includeProperties:"Product")
+            };
+
+            return View(orderVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public async Task<IActionResult> UpdateOrderDetails()
+        {
+           var orderHeaderFromDb = await _unitOfWork.OrderHeader.GetAsync(u => u.Id == orderVM.OrderHeader.Id);
+
+            orderHeaderFromDb.Name = orderVM.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = orderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.Street = orderVM.OrderHeader.Street;
+            orderHeaderFromDb.City = orderVM.OrderHeader.City;
+            orderHeaderFromDb.State = orderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = orderVM.OrderHeader.PostalCode;
+            if (!string.IsNullOrEmpty(orderVM.OrderHeader.Carrier)) {
+                orderHeaderFromDb.Carrier = orderVM.OrderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(orderVM.OrderHeader.TrackingNumber)) {
+                orderHeaderFromDb.TrackingNumber = orderVM.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Order Detail updated successfully";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id});
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
