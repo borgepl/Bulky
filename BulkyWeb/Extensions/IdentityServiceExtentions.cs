f6c5ce1f25f45ee2166d7dcda0501ec7b278e26c
borgepl <pdeborger@gmail.com>
2023-05-23 10:58:30 +0200

    Identity Configure options

--- BulkyWeb/Extensions/IdentityServiceExtentions.cs
+++ BulkyWeb/Extensions/IdentityServiceExtentions.cs
@@ -14,6 +14,15 @@ namespace BulkyWeb.Extensions
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>( options => {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 5;
+                // options.Password.RequireDigit = true;
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+                // options.SignIn.RequireConfirmedEmail = true;
+            });
+
             services.ConfigureApplicationCookie(options => {
                 options.LoginPath = $"/Identity/Account/Login";
                 options.LogoutPath = $"/Identity/Account/Logout";
